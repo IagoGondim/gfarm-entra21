9029c2234bc61cf8d1d1e3611cdbd5d4440f6295
beatrizzcoan <136799922+beatrizzcoan@users.noreply.github.com>
2023-12-01 03:05:32 -0300

    endereco

---
+++ Endereco.java
@@ -0,0 +1,30 @@
+package endereco;
+
+public class Endereco {
+    public String getLogradouro() {
+        return null;
+    }
+    public int getNumero() {
+        return 0;
+    }
+    public String getBairro() {
+        return null;
+    }
+    public Object getCidade() {
+        return null;
+    }
+    public void setId(int int1) {
+    }
+    public int getId() {
+        return 0;
+    }
+    public void setLogradouro(String string) {
+    }
+    public void setNumero(int int1) {
+    }
+    public void setBairro(String string) {
+    }
+	public void setCidade(String string) {
+	}
+
+}
