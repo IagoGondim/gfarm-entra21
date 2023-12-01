3cde2eac332331e7de4eab407955aebae01b7991
beatrizzcoan <136799922+beatrizzcoan@users.noreply.github.com>
2023-12-01 10:07:58 -0300

    agricola

---
+++ AtividadeAgricola.java
@@ -0,0 +1,19 @@
+package agricola;
+
+import java.sql.Timestamp;
+
+public class AtividadeAgricola {
+    public String getDescricao() {
+        return null;
+    }
+    public Timestamp getDataDaAtividade() {
+        return null;
+    }
+    public void setId(int int1) {
+    }
+    public void setDescricao(String string) {
+    }
+    public void setDataDaAtividade(Timestamp timestamp) {
+    }
+
+}
