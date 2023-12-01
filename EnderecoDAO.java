1fae29cfcee65988a7c5587bce7143f52079ba6e
beatrizzcoan <136799922+beatrizzcoan@users.noreply.github.com>
2023-12-01 03:07:12 -0300

    endereco

---
+++ EnderecoDAO.java
@@ -0,0 +1,11 @@
+package endereco;
+
+import java.util.List;
+
+public interface EnderecoDAO {
+    Endereco salvarEndereco(Endereco endereco);
+    Endereco atualizarEndereco(Endereco endereco);
+    void deletarEndereco(int enderecoId);
+    Endereco obterEnderecoPorId(int enderecoId);
+    List<Endereco> obterTodosEnderecos();
+}
